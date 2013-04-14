e6765c903b53e29b622f12565d12415d1545609f
Emile Swarts <emile.swarts123@gmail.com>
2013-04-14 14:27:22 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyformybrain::Application.config.secret_token = '3fcdf9145982318856ed6091526e6a502eb5ca2874101a0e96c9167d8251bbe77e958f844df5891d1377ee6118f42f0163535bd22860c0b7f850def9f40c3f4b'
